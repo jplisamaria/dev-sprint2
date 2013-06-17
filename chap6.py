d344861f980178c229c9f057ae09287c8277033b
jplisamaria <jplisamaria@gmail.com>
2013-06-17 15:46:41 -0400

    Exercise 6.6: Palindrome

--- chap6.py
+++ chap6.py
@@ -1,9 +1,58 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name:Lisa-Maria Mehta
 
 
-# Ex. 6.6
+# ******************
+# ** Exercise 6.6 **
+# ******************
+# Palindrome: Function definitions
+# ----------------------------------
+
+def first(word):
+	"""Returns first element of a string"""
+	return word [0]
+
+def last(word):
+	"""Returns last element of a string"""
+	return word [-1]
+
+def middle(word):
+	"""Takes a string as an argument, and returns same string
+	with the first and last elements truncated off."""
+	return word [1:-1]
 
 
+def is_palindrome (word):
+	"""Takes a string as an argument. Exits if the 
+	string has 0 length.  Returns True if it is a palindrome,
+	and False if it is not a palindrome."""
+	if len(word) == 0:
+		return
+	if (len(word) == 1) or ((len(word)==2) and (first(word)==last(word))):
+		return True
+	else:
+		if first(word) != last (word):
+			return False
+		return is_palindrome(middle(word))
 
-# Ex 6.8
+
+# ------------------------
+# Palindrome: Main Program
+# ------------------------
+
+print "Enter word (case matters):",
+word = raw_input ()
+
+if is_palindrome(word) == True:
+	print word, "is a palindrome."
+elif is_palindrome(word) == False:
+	print word, "is not a palindrome."
+else:
+	print "Error: This is not a word."
+
+
+# ******************
+# ** Exercise 6.8 **
+# ******************
+# Palindrome: Function definitions
+# --------------------------------
